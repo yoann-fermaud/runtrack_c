6b6a350bdf0ebfba031c82a473be96f2c381348a
yoann-fermaud <yoann.fermaud@laplateforme.io>
2023-11-28 15:04:32 +0100

    [ADD] runtrack_c d1-j3

---
+++ Jour01/Job03/str_is_digit.c
@@ -0,0 +1,10 @@
+int str_is_digit(char *str) {
+    int i = 0;
+    while (str[i] != '\0') {
+        if (str[i] < '0' || str[i] > '9') {
+            return 0;
+        }
+        i++;
+    }
+    return 1;
+}
